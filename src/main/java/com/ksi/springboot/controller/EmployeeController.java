a88db601bcdc0382c00902bd26334fb826974da7
Khandaker Shafiqul Islam <ksishafiq007@gmail.com>
2021-12-01 00:43:43 +0600

    Update EmployeeController.java
    Create employee add

--- src/main/java/com/ksi/springboot/controller/EmployeeController.java
+++ src/main/java/com/ksi/springboot/controller/EmployeeController.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,7 +14,7 @@ import com.ksi.springboot.model.Employee;
 import com.ksi.springboot.repository.EmployeeRepository;
 
 
-@CrossOrigin(origins = "http://localhost:3000/")
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/api/v1/")
 public class EmployeeController {
@@ -20,9 +22,14 @@ public class EmployeeController {
 	@Autowired
 	private EmployeeRepository employeeRepository;
 	
-	@GetMapping("/employees")
-	public List<Employee> getAllEmployees(){
+	@GetMapping("/employees")                         
+	public List<Employee> getAllEmployees(){                          // Get all employees
 		return employeeRepository.findAll();
+	}
+	
+	@PostMapping("/employees")
+	public Employee createEmployee(@RequestBody Employee employee) {              // Create Employee rest api
+		return employeeRepository.save(employee);
 	}
 
 }
