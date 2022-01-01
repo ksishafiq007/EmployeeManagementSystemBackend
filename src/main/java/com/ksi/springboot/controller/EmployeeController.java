d0de9a32354b2362c29ba500c0d8f15f2262d8df
Khandaker Shafiqul Islam <ksishafiq007@gmail.com>
2022-01-01 23:34:37 +0600

    return employee by id

--- src/main/java/com/ksi/springboot/controller/EmployeeController.java
+++ src/main/java/com/ksi/springboot/controller/EmployeeController.java
@@ -3,13 +3,16 @@ package com.ksi.springboot.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.ksi.springboot.exception.ResourceNotFoundException;
 import com.ksi.springboot.model.Employee;
 import com.ksi.springboot.repository.EmployeeRepository;
 
@@ -30,6 +33,13 @@ public class EmployeeController {
 	@PostMapping("/employees")
 	public Employee createEmployee(@RequestBody Employee employee) {              // Create Employee rest api
 		return employeeRepository.save(employee);
+	}
+	
+	@GetMapping("/employees/{id}")
+	public ResponseEntity<Employee> getEmployeeById(@PathVariable Long id){
+		Employee employee=employeeRepository.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("Employee not exis with id :" + id));
+		return ResponseEntity.ok(employee);
 	}
 
 }
