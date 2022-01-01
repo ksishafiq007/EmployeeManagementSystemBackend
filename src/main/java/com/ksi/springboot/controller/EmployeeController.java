83fd52a278c0fc56cf7bfd3215e039d60d4f0ace
Khandaker Shafiqul Islam <ksishafiq007@gmail.com>
2022-01-02 01:22:17 +0600

    update employee

--- src/main/java/com/ksi/springboot/controller/EmployeeController.java
+++ src/main/java/com/ksi/springboot/controller/EmployeeController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -35,11 +36,26 @@ public class EmployeeController {
 		return employeeRepository.save(employee);
 	}
 	
-	@GetMapping("/employees/{id}")
+	@GetMapping("/employees/{id}")                                           //get employee by id with rest api
 	public ResponseEntity<Employee> getEmployeeById(@PathVariable Long id){
-		Employee employee=employeeRepository.findById(id)
+		Employee employee = employeeRepository.findById(id)
 				.orElseThrow(() -> new ResourceNotFoundException("Employee not exis with id :" + id));
 		return ResponseEntity.ok(employee);
+	}
+	
+	@PutMapping("/employees/{id}")
+	public ResponseEntity<Employee> updateEmployee(@PathVariable Long id, @RequestBody Employee employeeDetails) {
+		
+		Employee employee = employeeRepository.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("Employee not exis with id :" + id));
+		
+		employee.setFirstName(employeeDetails.getFirstName());
+		employee.setLastName(employeeDetails.getLastName());
+		employee.setEmailId(employeeDetails.getEmailId());
+		
+		Employee updatedEmployee = employeeRepository.save(employee);
+		return ResponseEntity.ok(updatedEmployee);
+		
 	}
 
 }
